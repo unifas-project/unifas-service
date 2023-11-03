e02cb7ac21686165c89a7c32b3b0a85c3f7bab1e
absolutely2013 <changktc.0306@gmail.com>
2023-11-03 10:24:23 +0700

    project-structure commit

---
+++ unifas-service/src/main/java/com/example/unifasservice/UnifasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.unifasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifasServiceApplication.class, args);
+	}
+
+}
